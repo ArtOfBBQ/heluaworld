5c65bacb700804aee609ac3acabda937d5373778
Jelle Van den Eynde <40927974+ArtOfBBQ@users.noreply.github.com>
2020-06-09 03:03:25 +0900

    control our little vehicle

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
 local gameobjects = {}
 
+local pressingup = false
+local pressingdown = false
+local pressingright = false
+local pressingleft = false
+
 function love.load()
     local object = require('modules.object')
 
@@ -8,17 +13,67 @@ function love.load()
     images.tankgun = love.graphics.newImage("assets/images/tanktop.png")
     
     gameobjects[1] = object:new()
-    gameobjects[1].weapon_angle = 0.40
+    gameobjects[1].weapon_angle = gameobjects[1].angle
 end
 
+function love.keypressed( key, scancode, isrepeat )
+
+    if key == 'up' then pressingup = true end
+
+    if key == 'down' then pressingdown = true end
+
+    if key == 'right' then pressingright = true end
+
+    if key == 'left' then pressingleft = true end
+
+end
+
+function love.keyreleased( key, scancode, isrepeat )
+
+    if key == 'up' then pressingup = false end
+
+    if key == 'down' then pressingdown = false end
+
+    if key == 'right' then pressingright = false end
+
+    if key == 'left' then pressingleft = false end
+
+end
+
+
 function love.mousepressed(x, y, button, istouch)
+
+    if rotating == true and accelerating == true then
+        rotating = false
+    elseif rotating == false and accelerating == true then
+        accelerating = false
+    elseif rotating == false and accelerating == false then
+        rotating = true
+    elseif rotating == true and accelerating == false then
+        accelerating = true
+    end
+
+    -- if accelerating == false then accelerating = true else accelerating = false end
+    
 end
 
 function love.update(dt)
 
     local i = 1
-    
-    gameobjects[i]:rotate_right(0.02)
+
+    if pressingright then gameobjects[i]:rotate_right() end
+
+    if pressingleft then gameobjects[i]:rotate_left() end
+
+    if pressingup then gameobjects[i]:accelerate() end
+
+    if pressingdown then gameobjects[i]:decelerate(0.04) end
+
+    gameobjects[i].x = gameobjects[i].x + gameobjects[i].x_velocity
+    gameobjects[i].y = gameobjects[i].y + gameobjects[i].y_velocity
+
+    -- decelerate naturally
+    gameobjects[i]:decelerate(0.003)
 
 end
 
@@ -45,8 +100,12 @@ function love.draw()
             gameobjects[i].weapon_angle,
             1,
             1,
-            (images.tankgun:getWidth() / 2) - 30,
-            images.tankgun:getHeight() / 2)
+            (images.tankgun:getWidth() / 2),
+            (images.tankgun:getHeight() / 2) + 30)
     end
+
+    love.graphics.print("x velocity: " .. gameobjects[i].x_velocity, 700, 50)
+    love.graphics.print("y velocity: " .. gameobjects[i].y_velocity, 700, 60)
+    love.graphics.print("angle: " .. gameobjects[i].angle, 700, 70)
     
 end
--- modules/object.lua
+++ modules/object.lua
@@ -1,9 +1,18 @@
 object = {
     x = 200,
     y = 200,
+    x_velocity = 0.50,
+    y_velocity = 0.50,
+    can_move_while_rotating = false,
     angle = 0.5,
     angle2 = 0.6
 }
+
+-- allGameObjects[iPlayer].xVelocity += Math.sin(allGameObjects[iPlayer].angle) * 0.2;
+-- if (allGameObjects[iPlayer].xVelocity > 1) {allGameObjects[iPlayer].xVelocity = 1};
+-- allGameObjects[iPlayer].yVelocity += -Math.cos(allGameObjects[iPlayer].angle) * 0.2;
+-- if (allGameObjects[iPlayer].yVelocity > 0.7) {allGameObjects[iPlayer].yVelocity = 0.7};
+
 
 function object.fix_radians_bounds(self, angle_property_name)
 
@@ -16,7 +25,19 @@ end
 
 function object.rotate_left(self, increment)
     
-    increment = increment or 0.02
+    increment = increment or 0.005
+
+    self.angle = self.angle - increment
+    if self['weapon_angle'] ~= nil then self.weapon_angle = self.weapon_angle - increment end
+
+    self:fix_radians_bounds('angle')
+    self:fix_radians_bounds('weapon_angle')
+
+end
+
+function object.rotate_right(self, increment)
+    
+    increment = increment or 0.005
 
     self.angle = self.angle + increment
     if self['weapon_angle'] ~= nil then self.weapon_angle = self.weapon_angle + increment end
@@ -26,15 +47,40 @@ function object.rotate_left(self, increment)
 
 end
 
-function object.rotate_right(self, increment)
-    
-    increment = increment or 0.02
+function object.accelerate(self, increment)
 
-    self.angle = self.angle - increment
-    if self['weapon_angle'] ~= nil then self.weapon_angle = self.weapon_angle - increment end
+    increment = increment or 0.03
     
-    self:fix_radians_bounds('angle')
-    self:fix_radians_bounds('weapon_angle')
+    self.x_velocity = self.x_velocity + (math.sin(self.angle) * increment)
+    self.y_velocity = self.y_velocity - (math.cos(self.angle) * increment)
+
+end
+
+function object.decelerate(self, increment)
+
+    increment = increment or 0.02
+    
+    if self.x_velocity > 0 then
+        self.x_velocity = math.max(
+            -- self.x_velocity - (math.sin(self.angle) * increment),
+            self.x_velocity - increment,
+            0)
+    else
+        self.x_velocity = math.min(
+            -- self.x_velocity - (math.sin(self.angle) * increment),
+            self.x_velocity + increment,
+            0)
+    end
+    
+    if self.y_velocity > 0 then
+        self.y_velocity = math.max(
+            self.y_velocity - increment,
+            0)
+    else
+        self.y_velocity = math.min(
+            self.y_velocity + increment,
+            0)
+    end
 
 end
 
